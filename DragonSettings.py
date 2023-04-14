62852f32f2d78feac02a51483680332c8caf5960
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-14 12:25:05 +0200

    Create DragonSettings.py

---
+++ DragonSettings.py
@@ -0,0 +1,13 @@
+class Settings:
+    COLOR=0xa83232 # embed farbe
+    WINNERS=[
+        '733403498766401554', # kiwious
+        '547664017209032709', # brawny
+        '771108568509972490', # kuappi
+        '689558970796343380' # master sim
+        '696710421004025907', # limonaden tim
+        '742126810552074310', # nini
+        '647798346265657345', # vacuumboy
+        '731167863234363522', # niniunsinushi
+    ]
+    GIVEAWAY_TIME=5 # giveaway zeit in sekunden (die 5 sekunden sind temporär)

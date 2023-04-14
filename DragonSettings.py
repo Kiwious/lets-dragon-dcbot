5135e91f5dc0bfb09a3d79d7e10875e6ce92b410
Kiwi <84627732+Kiwious@users.noreply.github.com>
2023-04-14 12:44:41 +0200

    Add client permissions

--- DragonSettings.py
+++ DragonSettings.py
@@ -11,3 +11,56 @@ class Settings:
         '731167863234363522', # niniunsinushi
     ]
     GIVEAWAY_TIME=5 # giveaway zeit in sekunden (die 5 sekunden sind temporär)
+
+    # GRIEFING SYSTEM:
+
+    # Erforderliche Bot-Rechte (True => Erforderlich, False => Nicht erforderlich):
+    CLIENT_PERMISSIONS={
+        'add_reactions': True,
+        'administrator': True,
+        'attach_files': True,
+        'ban_members': True,
+        'change_nickname': True,
+        'connect': True,
+        'create_instant_invite': True,
+        'create_public_threads': True,
+        'create_private_threads': True,
+        'deafen_members': True,
+        'embed_links': True,
+        'external_emojis': True,
+        'external_stickers': True,
+        'kick_members': True,
+        'manage_channels': True,
+        'manage_emojis': True,
+        'manage_emojis_and_stickers': True,
+        'manage_events': True,
+        'manage_guild': True,
+        'manage_messages': True,
+        'manage_nicknames': True,
+        'manage_permissions': True,
+        'manage_roles': True,
+        'manage_threads': True,
+        'manage_webhooks': True,
+        'mention_everyone': True,
+        'moderate_members': True,
+        'move_members': True,
+        'mute_members': True,
+        'priority_speaker': True,
+        'read_message_history': True,
+        'read_messages': True,
+        'request_to_speak': True,
+        'send_messages': True,
+        'send_messages_in_threads': True,
+        'send_tts_messages': True,
+        'speak': True,
+        'stream': True,
+        'use_application_commands': True,
+        'use_embedded_activities': True,
+        'use_external_emojis': True,
+        'use_external_stickers': True,
+        'use_voice_activation': True,
+        'value': True,
+        'view_audit_log': True,
+        'view_channel': True,
+        'view_guild_insights': True
+    }
